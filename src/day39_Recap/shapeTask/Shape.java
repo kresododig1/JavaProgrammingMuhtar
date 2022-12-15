fb185c478d992a6c8844e36f4127cd8fe9fc14ba
Krešimir Dodig <kresododig@yahoo.com>
2022-12-15 08:22:34 +0100

    day39_Recap

---
+++ src/day39_Recap/shapeTask/Shape.java
@@ -0,0 +1,50 @@
+package day39_Recap.shapeTask;
+
+public class Shape {
+
+    private String name;
+
+    public String getName() {//getter
+        return name;
+    }
+
+    public void setName(String name) {//setter
+        if (name==null){
+            System.err.println("Name can not be null!");
+            System.exit(1);// 1 means something went wrong!
+        }
+
+        if(name.isEmpty() || name.isBlank()){
+            System.err.println("Invalid name");
+            System.exit(1);
+        }
+        this.name = name;
+    }
+
+    public Shape(String name) {//Constructor
+       setName(name);
+    }
+
+    public double area(){//method
+        return 0;
+
+    }
+
+    public double perimeter(){//method
+        return 0;
+    }
+}
+/*
+
+Square extends Shape:
+	variables:
+		side;
+
+	Encapsulate the field
+
+	Add a constructor to set the filed
+
+	area(): side * side
+	perimeter(): side * 4
+	toString(): side, area, perimeter
+ */
